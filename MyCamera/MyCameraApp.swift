6db77a3480a9543f3a142e0b96e20618650409de
nogu66 <ezochi66@gmail.com>
2023-05-27 20:26:10 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by YutaNoguchi on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
